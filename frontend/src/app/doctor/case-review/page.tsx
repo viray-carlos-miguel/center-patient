79a337da8ae1c6b2bc562a380491c56762d74438
viray-carlos-miguel <carlosviray2004@gmail.com>
2026-03-09 10:40:59 +0800

    fix symptoms

--- frontend/src/app/doctor/case-review/page.tsx
+++ frontend/src/app/doctor/case-review/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30b19f23cd81362c64d26cc5d71ac066d4b93c5bea088fc4f57a074852030170
-size 55303
+oid sha256:b6008dedce0b90ce47cf0749e0422b6fc26b260d19136398974df9e34eeec2aa
+size 65234
